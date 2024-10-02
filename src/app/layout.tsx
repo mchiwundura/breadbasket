7429e091d9960cce6776dfdb200930cc8bcc8475
mchiwundura <mchiwundura@protonmail.com>
2024-10-02 22:10:56 +0200

    created project, made layout, navbar and footer, started work on index page with billboard component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Navigation from '../components/navigation'
+import Footer from '../components/footer'
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+	<Navigation/>
         {children}
+	<Footer/>
       </body>
     </html>
   );
